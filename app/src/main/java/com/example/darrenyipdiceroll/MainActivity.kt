8e938977647e01d390f6152e724e145c123f29ba
darrenyip96 <51224609+darrenyip96@users.noreply.github.com>
2020-06-26 08:39:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/darrenyipdiceroll/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.darrenyipdiceroll
+
+import android.media.Image
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
+        diceImage2 = findViewById(R.id.dice_image2)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+
+        val randomInt1  = (1..6).random()
+        val randomInt2  = (1..6).random()
+        val sumInt = randomInt1 + randomInt2
+
+        val drawableResource = when (randomInt1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+
+        }
+
+        val drawableResource2 = when (randomInt2){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
+
+        textView.text = sumInt.toString()
+
+
+
+    }
+
+}
+
+
+
